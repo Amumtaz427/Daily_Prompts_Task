627e6c27538c3b41395aea2ee867fe9dd2bd152f
Amumtaz427 <ahmadmumtaz427@gmail.com>
2024-12-22 21:19:59 +0530

    first commit

---
+++ Prompt_1/script.js
@@ -0,0 +1,44 @@
+var m = 12;
+const s = 12;
+console.log(m);
+console.log(s);
+alert("Plaese on the Network");
+console.error("this is your folt");
+console.warn("This is not Your folt");
+alert("There are two Datatypes (1):- Primitive Datatypes , (2):- Refrences");
+ 
+// Conditional Statement
+var num1= Number(prompt("Enter first Number"));
+var num2 = Number(prompt("Enter 2nd Number"));
+var num3 = Number(prompt("Enter 3rd Number"));
+
+if(num1>num2){
+    console.log("First is beggest");
+}
+else if(num2>num3){
+    console.log(" 2nd  is beggest");
+}
+else if(num1>num3){
+    console.log("First is beggest");
+}else{
+    console.log(" 3rd is beggest");
+}
+
+var m1= Number(prompt("Enter first Number"));
+if(m1>10){
+    console.log("Namastee India");
+}else{
+    console.log("Hello Countries ");
+}
+
+
+// Loops Here! for foreach
+for(var i = 0;i<11; i++ ){
+    console.log(i);
+}
+
+
+
+
+
+
